00f389726fdaf9abd86df8764dd629e40255a392
Scott Spencer <wsspence@ncsu.edu>
2016-11-25 19:45:52 -0500

    why won't this compile....

---
+++ Project 3/test/edu/ncsu/csc216/todolist/ui/TaskEditPaneTest.java
@@ -0,0 +1,14 @@
+package edu.ncsu.csc216.todolist.ui;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TaskEditPaneTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Project 3/test/edu/ncsu/csc216/todolist/ui/TaskTableModelTest.java
@@ -0,0 +1,14 @@
+package edu.ncsu.csc216.todolist.ui;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TaskTableModelTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
